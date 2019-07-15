80c9d2e1084674b8ce43394bee618a59680b6ab7
Eltrac <45323514+BigCoke233@users.noreply.github.com>
2019-07-15 17:06:46 +0800

    plus 说说动态页面

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@
 function Dark() {
 	$("body").toggleClass("body-dark");
 }
+var h = myDate.getHours();
+if !(h > 6 && h < 19 && hasClass('body-dark')) {
+	Dark();
+}
 //GoTop
 function GoTop() {
 	$('body,html').animate({scrollTop:0},500);
