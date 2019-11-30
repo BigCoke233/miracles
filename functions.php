9b1058952f4b473df92c1157ccb4153bb663b20c
Eltrac <45323514+BigCoke233@users.noreply.github.com>
2019-11-30 15:13:29 +0800

    🔍 Add: 文章浏览数统计

--- functions.php
+++ functions.php
@@ -44,3 +44,34 @@ function themeVersion() {
     $info = Typecho_Plugin::parseInfo(__DIR__ . '/index.php');
     return $info['version'];
 }
+
+/**
+ *  统计文章浏览数
+ *  from:http://docs.qqdie.com/
+ */
+function get_post_view($archive) {
+    $cid    = $archive->cid;
+    $db     = Typecho_Db::get();
+    $prefix = $db->getPrefix();
+    if (!array_key_exists('views', $db->fetchRow($db->select()->from('table.contents')))) {
+        $db->query('ALTER TABLE `' . $prefix . 'contents` ADD `views` INT(10) DEFAULT 0;');
+        echo 0;
+        return;
+    }
+    $row = $db->fetchRow($db->select('views')->from('table.contents')->where('cid = ?', $cid));
+    if ($archive->is('single')) {
+ $views = Typecho_Cookie::get('extend_contents_views');
+        if(empty($views)){
+            $views = array();
+        }else{
+            $views = explode(',', $views);
+        }
+if(!in_array($cid,$views)){
+       $db->query($db->update('table.contents')->rows(array('views' => (int) $row['views'] + 1))->where('cid = ?', $cid));
+array_push($views, $cid);
+            $views = implode(',', $views);
+            Typecho_Cookie::set('extend_contents_views', $views); //记录查看cookie
+        }
+    }
+    echo $row['views'];
+}
