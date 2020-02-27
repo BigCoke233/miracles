503dbc09d86c6f213581a4c9dbd824f75a5f7d9f
Eltrac Koalar <hi@guhub.cn>
2020-02-27 11:01:19 +0800

    :pancakes: Fix: 补上漏掉的 js

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,11 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-    <!-- 结束 pjax-container --></div>
+    </div><!-- 结束 pjax-container -->
+	<!-- Footer Information -->
     <footer>
       <div class="footer-info footer-dark">
-		<p class="copyright"><?php $this->options->footerEcho(); ?>
-		Powered by <a href="http://typecho.org">Typecho</a> | Theme <a href="https://guhub.cn/p/miracles.html">Miracles</a> by <a href="https://guhub.cn">Eltrac</a><br>
-		&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->SiteUrl(); ?>"><?php $this->options->title(); ?></a>  All rights reserved.<br>
+		<?php $this->options->footerEcho(); ?>
+		<p class="copyright">Powered by <a href="http://typecho.org">Typecho</a> | Theme <a href="https://guhub.cn/p/miracles.html">Miracles</a> by <a href="https://guhub.cn">Eltrac</a><br>
+		Copyright &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->SiteUrl(); ?>"><?php $this->options->title(); ?></a>, All rights reserved.<br>
         <?php 
 		echo $this->options->build_time?"记录已延续了 <span id=\"build-time\"></span>":"";
 		if($faces=$this->options->anime_face){
@@ -28,7 +29,7 @@
 	<script>
 	var allowNavAero = <?php if($this->options->navAero):?>false<?php else:?>true<?php endif;?>;
 	var siteurl = '<?php $this->options->SiteUrl() ;?>';
-	var owoJson = '<?php Utils::indexTheme("assets/OwO.json"); ?>';
+	var owoJson = '<?php Utils::indexTheme('assets/OwO.json'); ?>';
 	<?php if($this->options->pjax && $this->options->pjax!=0) :?>var loadPjax = true;
     beforePjax = function() {NProgress.start();};
 	afterPjax = function() {owoLoad();<?php $this->options->pjax_complete(); ?>};<?php endif; ?></script>
