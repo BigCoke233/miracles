91bf4aef1f89e354ea7888e6c3435fc768ee807d
Eltrac Koalar <hi@guhub.cn>
2020-03-09 10:48:05 +0800

    📜 Update: 更改版本号

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * 
  * @package     Miracles
  * @author      Eltrac
- * @version     1.4.0
+ * @version     1.4.1
  * @link        https://guhub.cn
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
