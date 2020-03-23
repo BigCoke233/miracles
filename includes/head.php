a2c5f9a6563d6cb677bcd36cb476fcae55809f89
Eltrac Koalar <hi@guhub.cn>
2020-03-23 16:55:49 +0800

    ☀️ Perf: 修改日间模式的图标

--- includes/head.php
+++ includes/head.php
@@ -17,8 +17,7 @@
             $description = $this->fields->excerpt;
     }else{
         $description = Helper::options()->description;
-    }
-    ?>
+    }?>
     <title><?php Contents::title($this); ?></title>
     <meta itemprop="name" content="<?php Contents::title($this); ?>"/>
     <meta itemprop="image" content="<?php Utils::indexTheme('favicon.ico'); ?>" />
@@ -47,7 +46,7 @@
 	<!-- icon font -->
 	<link rel="stylesheet" href="//at.alicdn.com/t/font_1165190_n6ylpatlse.css" />
 	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Noto+Sans+SC:300|Noto+Serif+SC:300&display=swap" rel="stylesheet">
+	<link href="//at.alicdn.com/t/font_1165190_4ff54hxh485.css" rel="stylesheet">
 	<style>.body-dark .pio-action .pio-home{background-image: url(<?php Utils::indexTheme('images/icons/home.png'); ?>);}.body-dark .pio-action .pio-close{background-image: url(<?php Utils::indexTheme('images/icons/remove.png'); ?>);}.body-dark .pio-action .pio-skin{background-image: url(<?php Utils::indexTheme('images/icons/skin.png'); ?>);}.body-dark .pio-action .pio-info{background-image: url(<?php Utils::indexTheme('images/icons/info.png'); ?>);}.body-dark .pio-action .pio-night{background-image: url(<?php Utils::indexTheme('images/icons/night.png'); ?>);}@media (min-width:992px) {.search{background-image:url(<?php Utils::indexTheme('images/search.png'); ?>);}.login{background-image:url(<?php Utils::indexTheme('images/login.png'); ?>)}}.search {background-position: bottom right;background-repeat: no-repeat}.login {background-position: bottom left;background-repeat:no-repeat}<?php $this->options->cssEcho(); ?></style>
   </head>
   <body class="<?php if($this->options->bodyFonts && $this->options->bodyFonts=1): ?>body-serif<?php endif; ?>">
