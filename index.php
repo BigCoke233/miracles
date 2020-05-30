554296651d09a8609681487d49c22c2571947f65
BigCoke233 <hi@guhub.cn>
2020-05-30 15:57:49 +0800

    :bookmark: Update: 更改版本号

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * 
  * @package     Miracles
  * @author      Eltrac
- * @version     1.5.4
+ * @version     1.5.5
  * @link        https://guhub.cn
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
